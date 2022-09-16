e6025089b7792c55de2a3945ab99994086f919f1
Igor Golub <89035564569@mail.ru>
2022-09-16 20:04:34 +0300

    создал массив и заполнил его, создал пустой массив, добавил переменную счетчик букв

--- final_verification_work.cs
+++ final_verification_work.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" };
+string[] miniArray = new string[4];
+int letter_counter = 0;
