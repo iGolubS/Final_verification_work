a943e5da893ef38ed74e0cc70bee639247705856
Igor Golub <89035564569@mail.ru>
2022-09-16 21:20:57 +0300

    доработал финальную версию, ввел еще одну переменную и избавился от одного for

--- final_verification_work.cs
+++ final_verification_work.cs
@@ -1,20 +1,14 @@
 ﻿string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" };
-string[] miniArray = new string[4];
+string[] miniArray = new string[stringArray.Length];
 int letter_counter = 0;
-for (int j = 0; j < stringArray.Length; j++)
+int size = 3;
+for (int i = 0; i<stringArray.Length; i++)
 {
-    string input = stringArray[j];
-    
-    for (int i = 0; i < input.Length; i++)
+    if (stringArray[i].Length <=size)
     {
-        letter_counter++;
+        miniArray[letter_counter]=stringArray[i];  
+        letter_counter++;   
     }
-   
-    if (letter_counter <= 3)
-    {
-        miniArray[j] = stringArray[j];
-    }
-    letter_counter = 0;
 }
 Console.Write($"[{String.Join(" ", stringArray)}] -> ");
 Console.Write($"[{String.Join(" ", miniArray)}]");
