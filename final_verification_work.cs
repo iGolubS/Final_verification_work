5e0a8f76594d907656a90a5ed7cec26d854a4845
Igor Golub <89035564569@mail.ru>
2022-09-16 20:10:08 +0300

    создал перебор слов первого массива, затем каждое слова разобрал на буквы при помощи счетчика, если счетчик дает три буквы или меньше - добавляем слово в пустой массив, обнуляем счетчик и переходим к следующему слову

--- final_verification_work.cs
+++ final_verification_work.cs
@@ -1,3 +1,18 @@
 ﻿string[] stringArray = new string[4] { "Hello", ":)", "три", "четыре" };
 string[] miniArray = new string[4];
 int letter_counter = 0;
+for (int j = 0; j < stringArray.Length; j++)
+{
+    string input = stringArray[j];
+    
+    for (int i = 0; i < input.Length; i++)
+    {
+        letter_counter++;
+    }
+   
+    if (letter_counter <= 3)
+    {
+        miniArray[j] = stringArray[j];
+    }
+    letter_counter = 0;
+}
